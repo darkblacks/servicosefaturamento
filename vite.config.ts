53c455e02b1c9e6ca22b566410a7b73c9e42a634
darkblacks <victorferreira.s0307@gmail.com>
2026-04-22 16:31:27 -0300

    analiseabastecimento 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Função que separa automaticamente as bibliotecas do node_modules
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Cria um chunk separado para as libs grandes, ajudando na performance
+            if (id.includes('xlsx')) return 'vendor-excel';
+            if (id.includes('recharts')) return 'vendor-charts';
+            return 'vendor'; // Todo o resto das libs vai para o vendor geral
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 2000, // Aumenta o limite para não ficar apitando alerta chato
+  },
 })
